6560aec6b12a16adc71686f4fcdf2bd8e93bfcf3
OREOREOREOREORE <114270949+OREOREOREOREORE@users.noreply.github.com>
2025-12-19 23:37:47 +0800

    a real dotfiles

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " " 
+
+vim.cmd(":hi statusline guibg=NONE")
+
+require('config.gset')
+require('config.kmap')
+
---
+++ nvim/lua/config/gset.lua
@@ -0,0 +1,8 @@
+
+vim.wo.number = true
+vim.opt.wrap = false 
+vim.opt.swapfile = false
+vim.o.tabstop = 4
+
+vim.cmd(":hi statusline guibg=NONE")
+
